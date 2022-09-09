4a520b00420347ec961c3325490e2937e3606d24
joshuadecano <joshuadecano@gmail.com>
2022-09-08 21:49:55 -0700

    GITGUARDIONEOUS

--- testeroni.py
+++ testeroni.py
@@ -3,7 +3,7 @@ def get_database():
     import pymongo
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://dev-morgan-admin:oxsuiNupoeJEqED1@cluster0.gurz1.mongodb.net/test"
+    CONNECTION_STRING = "paste link here"
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     from pymongo import MongoClient
