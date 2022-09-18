23c8a9195d591ac096c7b30993849eca91fd5d52
krez-sch <Scott.Herron@Student.csulb.edu>
2022-09-17 22:08:28 -0700

    Updated Hash
    Added custom hash algorithm using time as the hashing salt.

--- image_hash.py
+++ image_hash.py
@@ -1,8 +1,21 @@
 import hashlib
+from datetime import datetime
 
-# Generate hash from input
+# Generate md5 hash from input
 def generateHash(dataToHash):
     dataEncoded = str(dataToHash).encode("utf-8")
     md5Hash = hashlib.md5(dataEncoded)
     return md5Hash.hexdigest()
+
+
+# Custom hash algorithm using the current time as a salt
+def generateHash2(dataToHash):
+    mask = 0xFFFFFFFF
+    A = int(dataToHash)
+    B = int(datetime.now().timestamp() * 1000)
+    C = (A ^ B) & mask
+    D = C << 16
+    E = C ^ D
+    F = hex(E & mask)
+    return F[2:]
 
