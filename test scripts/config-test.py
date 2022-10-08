bfb031d303fd0c4bbd035b78fce245b8d27cfc56
krez-sch <Scott.Herron@Student.csulb.edu>
2022-10-08 15:46:17 -0700

    Update config-test.py
    Cleaned up variable names and formatting. Made easier to copy into other files as needed.

--- test scripts/config-test.py
+++ test scripts/config-test.py
@@ -1,26 +1,34 @@
-import configparser
-
 # Used as the basis for the twitter.py config changes.
 # This can be modified as needed for other files.
+
+import configparser
+
+# Disable interpolation so strings can be parsed literally
+# Important for non-alphanumeric characters
 config = configparser.ConfigParser(interpolation=None)
 file = 'confidential.ini'
 
-# Initialize blank values
-# ONLY add them in the unshared .ini file
-default = {'bearer_token': '',
-           'consumer_key': '',
-           'consumer_secret': '',
-           'access_token': '',
-           'access_token_secret': ''}
+# Section to read from the config, should be changed to Twitter, Discord, etc
+section = 'Test'
+
+# Initialize blank values for the needed keys
+# ONLY add values in the unshared .ini file
+default = {
+    'key1': '',
+    'key2': '',
+    'key3': ''
+}
 
 config.read(file)
-if 'Twitter' not in config:
-    config['Twitter'] = default
+
+# If the section doesn't exist, append the default values to the file and exit
+if section not in config:
+    config[section] = default
     with open(file, 'w') as configfile:
         config.write(configfile)
     
-    raise ValueError("Twitter tokens not found in %s file. Please add them before running Morgan." % file)
+    raise ValueError(f"{section} tokens not found in {file} file. Please add them before running Morgan.")
 
-for key, value in config['Twitter'].items():
+# Will print the existing INI values
+for key, value in config[section].items():
     print(key, ':', value)
-
