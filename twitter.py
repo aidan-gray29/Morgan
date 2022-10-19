ac420160fa3f3e136ba33e5e682221926290083b
dezrezy <48598484+dezrezy@users.noreply.github.com>
2022-10-18 17:18:17 -0700

    added functionality for sending/receiving dms

--- twitter.py
+++ twitter.py
@@ -1,4 +1,4 @@
-import tweepy, configparser, sys
+import tweepy, configparser, sys, pymongo
 from time import sleep
 from os import listdir
 from os.path import isfile, join
@@ -19,28 +19,40 @@ def tweepy_cred():
     return tweepy.API(auth)
 
 # Posts a GIF based on the emotion provided
-def tweet_reaction(api, emotion):
-    if emotion == "angry":
-        filename = "./ReactionGIFs/dino_angry.gif"
-        status = "Testing Angry GIF Post"
-    elif emotion == "happy":
-        filename = "./ReactionGIFs/dino_happy.gif"
-        status = "Testing Happy GIF Post"
-    elif emotion == "bored":
-        filename = "./ReactionGIFs/dino_bored.gif"
-        status = "Testing Bored GIF Post"
-    elif emotion == "sad":
-        filename = "./ReactionGIFs/dino_sad.gif"
-        status = "Testing Sad GIF Post"
-    elif emotion == "confused":
-        filename = "./ReactionGIFs/dino_confused.gif"
-        status = "Testing Confused GIF Post"
+def tweet_reaction(emotion):
+    switch= {
+        'angry': './ReactionGIFs/dino_angry.gif',
+        'happy': './ReactionGIFs/dino_happy.gif',
+        'bored': './ReactionGIFs/dino_bored.gif',
+        'sad': './ReactionGIFs/dino_sad.gif',
+        'confused': './ReactionGIFs/dino_confused.gif'
+    }
+    status = 'Testing GIF Post'
     try:
-        api.update_status_with_media(filename=filename,status=status)
+        auth = tweepy_cred()
+        auth.update_status_with_media(filename=switch.get(emotion, "Invalid"),status=status)
         print("Tweeted!")
         sleep(900)
     except Exception as e:
         print("Encountered error! Error details: %s"%str(e))
+
+# sends a direct message to user
+def send_dm(user_id, message):
+    auth = tweepy_cred()
+    auth.send_direct_message(recipient_id=user_id, text=message)
+    
+# receives last 5 direct messages
+# note: 5 can be changed to some arbitrary number
+def receive_dm():
+    auth = tweepy_cred()
+    messages = auth.get_direct_messages(count=5)
+    for message in reversed(messages):
+        sender_id = message.message_create["sender_id"]
+        text = message.message_create["message_data"]["text"]
+        usernames = {
+            sender_id : text,
+            }
+    return messages
 
 if __name__ == "__main__":
     config = configparser.ConfigParser(interpolation=None)
@@ -64,4 +76,5 @@ if __name__ == "__main__":
             config.write(configfile)
         raise ValueError(f"{section} tokens not found in {file} file. Please add them before running Morgan.")
 
-    tweet_reaction(tweepy_cred())
+    tweet_reaction('angry')
+    # receive_texts()
